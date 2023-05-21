694b1121ab5a658114312db8f081a173904e742a
pjames <pjames@gmail.com>
2023-05-21 22:06:27 +0700

    Added konga secret

--- initial-secrets.sh
+++ initial-secrets.sh
@@ -25,4 +25,18 @@ do
   fi
 done
 
-cp ${TMP_FILE} ${DST_FILE} 
+export $(xargs <.env)
+
+SRC_FILE=00-configs/konga-users.cfg
+CFG_FILE=konga-users.cfg
+cp ${SRC_FILE} ${CFG_FILE}
+sed -i "s#<<KONGA_ADMIN_USER>>#${KONGA_ADMIN_USER}#g" ${CFG_FILE}
+sed -i "s#<<KONGA_ADMIN_PASSWD>>#${KONGA_ADMIN_PASSWD}#g" ${CFG_FILE}
+echo "  KONGA_USERS_CONFIG: $(cat "${CFG_FILE}" | base64 -w0)" >> ${TMP_FILE}
+
+SRC_FILE=00-configs/konga-nodes.cfg
+CFG_FILE=konga-nodes.cfg
+cp ${SRC_FILE} ${CFG_FILE}
+echo "  KONGA_NODES_CONFIG: $(cat "${CFG_FILE}" | base64 -w0)" >> ${TMP_FILE}
+
+cp ${TMP_FILE} ${DST_FILE}
