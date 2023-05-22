aefef65ec12ed1327d7d4481ba129d751a032388
pjames <pjames@gmail.com>
2023-05-22 16:46:22 +0700

    Fixed argocd file logic

--- initial-argocd.sh
+++ initial-argocd.sh
@@ -3,8 +3,9 @@
 export $(xargs <.env)
 
 SRC_FILE=00-configs/addons-argocd-tpl.yaml
+TMP_FILE=addons-argocd-tpl.yaml
 DST_FILE=addons/addons-argocd.yaml
 
-cp ${SRC_FILE} ${DST_FILE}
-
-sed -i "s#<<ENV>>#${ENV}#g" ${DST_FILE}
+cp ${SRC_FILE} ${TMP_FILE}
+sed -i "s#<<ENV>>#${ENV}#g" ${TMP_FILE}
+cp ${TMP_FILE} ${DST_FILE}
