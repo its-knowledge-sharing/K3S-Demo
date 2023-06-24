d1efac3a7c4ee37a383a0dd1d4e12313b50dee10
pjames <pjames@gmail.com>
2023-06-24 16:12:35 +0700

    Removed unused code

--- initial-secrets.sh
+++ initial-secrets.sh
@@ -26,27 +26,4 @@ do
   fi
 done
 
-export $(xargs <.env)
-
-SRC_FILE=00-configs/konga-users.cfg
-CFG_FILE=konga-users.cfg
-cp ${SRC_FILE} ${CFG_FILE}
-sed -i "s#<<KONGA_ADMIN_USER>>#${KONGA_ADMIN_USER}#g" ${CFG_FILE}
-sed -i "s#<<KONGA_ADMIN_PASSWD>>#${KONGA_ADMIN_PASSWD}#g" ${CFG_FILE}
-echo "  KONGA_USERS_CONFIG: $(cat "${CFG_FILE}" | base64 -w0)" >> ${TMP_FILE}
-
-SRC_FILE=00-configs/konga-nodes.cfg
-CFG_FILE=konga-nodes.cfg
-cp ${SRC_FILE} ${CFG_FILE}
-echo "  KONGA_NODES_CONFIG: $(cat "${CFG_FILE}" | base64 -w0)" >> ${TMP_FILE}
-
-KEY_FILE=.gar-sa.json
-echo "  GAR_PASSWORD: $(cat "${KEY_FILE}" | base64 -w0)" >> ${TMP_FILE}
-
-SETTING_FILE=.appsetting-promrub-scb.json
-echo "  APP_SETTING_SCB: $(cat "${SETTING_FILE}" | base64 -w0)" >> ${TMP_FILE}
-
-SETTING_FILE=.appsetting-promjodd-carpark.json
-echo "  APP_SETTING_CARPARK_API: $(cat "${SETTING_FILE}" | base64 -w0)" >> ${TMP_FILE}
-
 cp ${TMP_FILE} ${DST_FILE}
